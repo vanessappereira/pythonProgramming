d5405c1134af9c04a802c9e26e37106137c70c4a
vanessappereira <vanessappereira@outlook.pt>
2024-06-05 19:52:01 +0100

    Update Django

--- DjangoProject1/app/views.py
+++ DjangoProject1/app/views.py
@@ -1,16 +1,19 @@
 import datetime
 from django.shortcuts import render
 from .models import Name
-nomes = ["Bubbles","Blossom","Buttercup", "Mojojojo","Professor Utonium"]
-cidades = []
+
+NAMES = ["Bubbles","Blossom","Buttercup", "Mojojojo","Professor Utonium"]
+CITIES = []
+
 # Create your views here.
 def response(request):
     #names = Name.objects.all()
-    names = nomes
-    cities = cidades
+    names = NAMES
+    cities = CITIES
     message = "Welcome to the Webpage!"
     current_year = datetime.date.today().year
     
     data_to_send = { 'names': names, 'cities': cities , 'message':message, 'current_year': current_year}
+
     
     return render(request, 'name.html', data_to_send)
